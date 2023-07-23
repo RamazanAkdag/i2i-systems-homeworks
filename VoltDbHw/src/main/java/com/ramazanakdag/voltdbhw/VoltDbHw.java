54fe4b2858649940fb1d1313ef255f3c4728bd56
RamazanAkdag <akdagramazan586@gmail.com>
2023-07-23 17:47:19 +0300

    .

---
+++ VoltDbHw/src/main/java/com/ramazanakdag/voltdbhw/VoltDbHw.java
@@ -0,0 +1,49 @@
+
+package com.ramazanakdag.voltdbhw;
+
+
+import java.sql.*;
+import java.io.*;
+
+public class VoltDbHw {
+
+    public static void main(String[] args) {
+ 
+        String driver = "org.voltdb.jdbc.Driver";
+        String url = "jdbc:voltdb://172.18.0.2:21212";
+        /*String userName = "ramo";
+        String password = "12345";*/
+        //String sql = "SELECT dialect FROM helloworld";
+        String sql = "SELECT * FROM SUBSCRIBER";
+        try {
+                // Load driver. Create connection.
+            Class.forName(driver);
+            Connection conn = DriverManager.getConnection(url/*,userName,password*/);
+            
+                // create a statement
+            Statement query = conn.createStatement();
+            ResultSet resultSet = query.executeQuery(sql);
+            while (resultSet.next()) {
+                int subscId = resultSet.getInt("SUBSC_ID");
+                String name = resultSet.getString("SUBSC_NAME");
+                String surname = resultSet.getString("SUBSC_SURNAME");
+                String msisdn = resultSet.getString("MSISDN");
+                int tariffId = resultSet.getInt("TARIFF_ID");
+                Date startDate = resultSet.getDate("START_DATE");
+
+                System.out.println("SUBSC_ID: " + subscId + ", SUBSC_NAME: " + name +
+                        ", SUBSC_SURNAME: " + surname + ", MSISDN: " + msisdn +
+                        ", TARIFF_ID: " + tariffId + ", START_DATE: " + startDate);
+            }
+            
+                 
+            resultSet.close();
+            query.close();
+            conn.close();
+
+        } catch (Exception e) {
+           e.printStackTrace();
+            //System.out.println(e.getMessage());
+        }
+    }
+}
